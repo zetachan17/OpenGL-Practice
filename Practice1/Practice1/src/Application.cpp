2c0b8813ae991f83d00adb76f106bef0e8bb985d
Tyler Zhu <67493075+zetachan17@users.noreply.github.com>
2021-02-27 21:33:41 -0500

    Initializing GLEW

--- Practice1/Practice1/src/Application.cpp
+++ Practice1/Practice1/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -16,8 +18,15 @@ int main(void)
         return -1;
     }
 
+    glewInit();
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
