070f4d8c8464538de40629dda0df3c5761b37152
Runze Zhu <z_runze@live.concordia.ca>
2021-07-13 23:29:59 -0400

    add glm

--- Practice1/Project1/Project1/src/Application.cpp
+++ Practice1/Project1/Project1/src/Application.cpp
@@ -11,16 +11,22 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+
+"out vec3 ourColor;\n"
+
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
+"in vec3 ourColor;\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"	FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 int main() {
@@ -99,9 +105,9 @@ int main() {
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f, // left
-	 0.5f, -0.5f, 0.0f, // right
-	 0.0f,  0.5f, 0.0f  // top
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // left
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // right
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f// top
 	};
 
 	
@@ -114,8 +120,12 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
